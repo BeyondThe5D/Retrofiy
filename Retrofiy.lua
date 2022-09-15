39934dac7d7004fb4ff8ba7d45d072b6ee67d0c7
BeyondThe5D <92761598+BeyondThe5D@users.noreply.github.com>
2022-09-15 22:10:37 +0100

    Update Retrofiy.lua

--- Retrofiy.lua
+++ Retrofiy.lua
@@ -26,7 +26,7 @@ local RetrofiyConfig = {
 	RetroWorkspace = true, -- [B] -- Uses old materials, disables terrain decoration, only allows brick colors and returns 2016 studs
 	RetroCharacters = true, -- [B] -- Displays health bars above the heads of characters & returns old oof sound
 	RetroChat = true, -- [B] -- If default chat is enabled it will convert it to the 2016 chat
-	BCOnly = false -- [O] -- Makes all premium players appear as BC players instead of it being random
+	BCOnly = false -- [O] -- Makes all premium players appear as BC players instead of it being User ID linked
 }
 
 if not game:IsLoaded() then
@@ -126,7 +126,6 @@ if RetrofiyConfig.RetroCoreGui then
 	}
 	local SpecialPlayers = {
 		[2601528367] = Icons["Developer"],
-		[2606489681] = Icons["YouTuber"],
 		[42049882] = "rbxassetid://10582975516"
 	}
 
